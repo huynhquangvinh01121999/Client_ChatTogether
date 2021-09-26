91f3e819bc13289c5586effa674df01a62c0fca1
VinhHuynh <huynhquangvinh01121999@gmail.com>
2021-09-26 19:37:23 +0700

    update deploy

--- src/components/Chat/InboxChat.js
+++ src/components/Chat/InboxChat.js
@@ -1,5 +1,4 @@
 import React, { useContext } from "react";
-import $ from "jquery";
 import { useSelector } from "react-redux";
 import { AuthContext } from "../../Context/AuthProvider";
 import { ChatContext } from "../../Context/ChatProvider";
